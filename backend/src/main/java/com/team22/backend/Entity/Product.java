58909b0781462d96f814974854cd5ffded8bc297
opalnuttanun <nuttanun.plap@gmail.com>
2018-12-19 19:19:40 +0700

    close#21 - ทำ Entity ของ Stocking

---
+++ backend/src/main/java/com/team22/backend/Entity/Product.java
@@ -0,0 +1,38 @@
+package com.okta.developer.demo.entity;
+import javax.persistence.*;
+import lombok.*;
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity  //บอกว่าเป็น class entity class ที่เก็บขอมูล
+@Data  // lombox จะสร้าง method getter setter ให้เอง
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name="Product") //ชื่อตาราง
+public class Product {
+    @Id  //  Annotations  @Id  บอกว่าเป็น  Primary  key
+    @SequenceGenerator(name="product_seq",sequenceName="product_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="product_seq")
+    @Column(name="Products_ID",unique = true, nullable = false)
+
+    private @NonNull Long prodId;
+
+    private @NonNull String productID;
+    private @NonNull String productName;
+    private @NonNull Integer productQuantity;
+    private @NonNull Date productDate;
+    private @NonNull Integer productPrice ;
+    private @NonNull String productImage ;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Type.class)
+    @JoinColumn(name = "typeIds", insertable = true)
+    private Type type;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Status.class)
+    @JoinColumn(name = "stateId", insertable = true)
+    private Status status;
+
+
+}
