204736e366a14dac9a062e2f6fc669d9d47adb5f
opalnuttanun <nuttanun.plap@gmail.com>
2019-01-24 23:25:32 +0700

    add controller เพิ่มสินค้า -close#14

---
+++ backend/src/main/java/com/team22/backend/Controller/ProductController.java
@@ -0,0 +1,64 @@
+package com.application.SE.controller;
+
+import com.application.SE.Entity.*;
+import com.application.SE.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class ProductController {
+
+    @Autowired
+    private DetailRepository detailRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private StatusRepository statusRepository;
+    @Autowired
+    private TypeRepository typeRepository;
+    @Autowired
+    private DescriptionRepository descriptionRepository;
+
+    @GetMapping(path = "/detail", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Detail> details() {
+        return detailRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping(path = "/type", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Type> types() {
+        return typeRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping(path = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Product> product() {
+        return productRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping(path = "/status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Status> status() {
+        return statusRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping(path = "/description", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<Description> description() {
+        return descriptionRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @PostMapping("/product/add/{productID}/{productName}/{productPrice}/{productQuantity}/{productDate}/{state}/{type}")
+    public Product newProduct(@RequestBody Product newProduct, @PathVariable String productID, @PathVariable String productName
+            , @PathVariable Integer productPrice, @PathVariable Integer productQuantity, @PathVariable Date productDate, @PathVariable Long state, @PathVariable Long type
+    ) {
+        Status setStatus = statusRepository.findByStateId(state);
+        Type setType = typeRepository.findByTypeIds(type);
+        newProduct.setProductIds(productID);
+        newProduct.setProductName(productName);
+        newProduct.setProductQuantity(productQuantity);
+        newProduct.setProductPrice(productPrice);
+        newProduct.setProductDate(productDate);
+  //      newProduct.setProductImg(productImg);/{productImg},@PathVariable String productImg
+        newProduct.setStatus(setStatus);
+        newProduct.setType(setType);
+        return productRepository.save(newProduct);
+    }
+}
