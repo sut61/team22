89dc8bbda3baa0d499fc53df8489d14f9b936091
sunvodz <sunvo3@gmail.com>
2019-02-03 03:23:53 +0700

    ทำ UI Customer -close #88

---
+++ frontend/src/app/service/signup.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignupService {
+
+  constructor(private http: HttpClient) {}
+
+  getCareer(): Observable<any> {
+      return this.http.get('//localhost:8080/career');
+  }
+  getProvince(): Observable<any> {
+    return this.http.get('//localhost:8080/province');
+}
+
+}
