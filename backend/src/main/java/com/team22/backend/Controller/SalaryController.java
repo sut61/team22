dc089f53707e7e2801537293fc0fc533b5196bc6
tungao06 <chayanunzaza@gmail.com>
2019-02-06 01:44:06 +0700

    Add SalaryController.java - close #122

---
+++ backend/src/main/java/com/team22/backend/Controller/SalaryController.java
@@ -0,0 +1,120 @@
+package com.team22.backend.Controller;
+
+import com.team22.backend.Entity.*;
+import com.team22.backend.Repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+
+class SalaryController {
+    @Autowired
+    private SalaryRepository salaryRepository;
+    @Autowired
+    private StaffRepository staffRepository;
+    @Autowired
+    private PositionRepository positionRepository;
+    @Autowired
+    private EducationRepository educationRepository;
+    @Autowired
+    private ExperienceRepository experienceRepository;
+    @Autowired
+    private PayerRepository payerRepository;
+
+    @Autowired
+    SalaryController(SalaryRepository salaryRepository,
+                    StaffRepository staffRepository,
+                    PositionRepository positionRepository,
+                    EducationRepository educationRepository,
+                    ExperienceRepository experienceRepository,
+                    PayerRepository payerRepository
+    ){
+        this.salaryRepository = salaryRepository;
+        this.staffRepository = staffRepository;
+        this.payerRepository = payerRepository;
+        this.educationRepository = educationRepository;
+        this.experienceRepository = experienceRepository;
+    }
+
+    @GetMapping("/salary2")
+    public Collection<Salary> salary2() {
+        return salaryRepository.findAll().stream().collect(Collectors.toList());
+    }
+    @GetMapping("/salary")
+        public Collection<Salary> salary() {
+            return salaryRepository.findAll().stream()
+                    .filter(this::isSalary)
+                    .collect(Collectors.toList());
+        }
+        private boolean isSalary(Salary salary){
+            return salary.getStaff().getStaffStatus().equals("UnPaid");
+        }
+    @GetMapping("/salary1")
+        public Collection<Salary> salary1() {
+            return salaryRepository.findAll().stream()
+                    .filter(this::isSalary1)
+                    .collect(Collectors.toList());
+        }
+        private boolean isSalary1(Salary salary){
+            return salary.getStaff().getStaffStatus().equals("Paid");
+        }
+    @GetMapping("/payer")
+    public Collection<Payer> payer() {
+        return payerRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @PostMapping("/salaryPost/{staffName}/{payerId}/{staffStatus}/{staffSalary}")
+    public Salary Salary1(
+        @PathVariable String   staffName,
+        @PathVariable Long   payerId,
+        @PathVariable String   staffStatus,        
+        @PathVariable int   staffSalary
+         ){
+        Salary salary = new Salary();
+        for(Long i=1L; i<9999L;i++) {
+            if(salaryRepository.findBySalaryId(i) == null) {
+                salary.setSalaryIds("SA"+i);
+                break;
+            }
+        }
+
+        Date datePay = new Date();
+        salary.setSalaryDate(datePay);
+
+        Staff staff1 = staffRepository.findByStaffName(staffName);
+        Payer payer1 = payerRepository.findByPayerId(payerId);
+        salary.setStaff(staff1);
+        salary.getStaff().setStaffStatus(staffStatus);
+        salary.getStaff().setStaffSalary(staffSalary);
+        salary.setPayer(payer1);
+    return salaryRepository.save(salary);
+    }
+
+    @PutMapping("/salary/{salaryId}/{staffId}/{staffStatus}/{salaryDate}")
+    Salary Salary2(
+            Salary newSalary,
+            @PathVariable String staffStatus,
+            @PathVariable Long staffId,
+            @PathVariable Long salaryId,
+            @PathVariable Date salaryDate
+            ) {
+        Staff staff1 = staffRepository.findByStaffId(staffId);
+        return salaryRepository.findById(salaryId)
+                .map(salary -> {
+                    salary.setSalaryDate(new Date());
+                    salary.getStaff().setStaffStatus(staffStatus);
+                    return salaryRepository.save(salary);
+                }
+                ).orElseGet(() -> {
+                    newSalary.setStaff(staff1);
+                    return salaryRepository.save(newSalary);
+                });
+    }
+    
+}
