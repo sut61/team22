0c6b0e886a52d594777dcae9987158b89c613ef9
tungao06 <chayanunzaza@gmail.com>
2019-02-06 23:54:18 +0700

    Add Salary.java - close #132

---
+++ backend/src/main/java/com/team22/backend/Entity/Salary.java
@@ -0,0 +1,42 @@
+package com.team22.backend.Entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.Collection;
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name="Salary")
+public class Salary {
+    @Id
+    @SequenceGenerator(name="salary_seq",sequenceName="salary_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="salary_seq")
+    @Column(name="Salary_ID",unique = true, nullable = false)
+
+    @NotNull
+    private  Long   salaryId;
+
+    @NotNull(message = "SalaryIds not null")
+    private  String salaryIds;
+
+    private  Date salaryDate;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Staff.class)
+    @JoinColumn(name = "staffId", insertable = true)
+    private Staff staff;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Payer.class)
+    @JoinColumn(name = "payerId", insertable = true)
+    private Payer payer;
+}
