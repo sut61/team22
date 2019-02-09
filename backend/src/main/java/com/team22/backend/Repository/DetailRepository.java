488f267bd116ac0a4ffc24447f6ef8b0bce1b33c
opalnuttanun <nuttanun.plap@gmail.com>
2019-02-09 07:02:53 +0700

    modified Repository - close#158

--- backend/src/main/java/com/team22/backend/Repository/DetailRepository.java
+++ backend/src/main/java/com/team22/backend/Repository/DetailRepository.java
@@ -2,8 +2,10 @@ package com.team22.backend.Repository;
 import com.team22.backend.Entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 public interface DetailRepository extends JpaRepository<Detail, Long>{
     Detail findByDetailIds(Long detailIds);
 }
--- backend/src/main/java/com/team22/backend/Repository/ProductRepository.java
+++ backend/src/main/java/com/team22/backend/Repository/ProductRepository.java
@@ -1,8 +1,8 @@
 package com.team22.backend.Repository;
 import com.team22.backend.Entity.*;
-        import org.springframework.data.jpa.repository.JpaRepository;
-        import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-        import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
